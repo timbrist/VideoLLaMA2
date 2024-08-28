80e5252f8eee7692a432a4ffd360c9ea6ad1cb08
Yan Shengheng <shengheng.yan@aalto.fi>
2024-08-28 15:50:59 +0300

    update download script

--- downloads/get_model.sh
+++ downloads/get_model.sh
@@ -1,15 +1,16 @@
+export DATA_FOLDER="/scratch/project_2010633/videollama2"
 
 wget https://huggingface.co/DAMO-NLP-SG/VideoLLaMA2-7B/resolve/main/tokenizer.model?download=true \
-    -O /scratch/project_2010633/videollama2/VideoLLaMA2-7B/tokenizer.model \
+    -O ${DATA_FOLDER}/VideoLLaMA2-7B/tokenizer.model \
 
 wget https://huggingface.co/DAMO-NLP-SG/VideoLLaMA2-7B/resolve/main/model-00004-of-00004.safetensors?download=true\
-    -O /scratch/project_2010633/videollama2/VideoLLaMA2-7B/model-00004-of-00004.safetensors \
+    -O ${DATA_FOLDER}/VideoLLaMA2-7B/model-00004-of-00004.safetensors \
 
 wget https://huggingface.co/DAMO-NLP-SG/VideoLLaMA2-7B/resolve/main/model-00003-of-00004.safetensors?download=true \
-    -O /scratch/project_2010633/videollama2/VideoLLaMA2-7B/model-00003-of-00004.safetensors \
+    -O ${DATA_FOLDER}/VideoLLaMA2-7B/model-00003-of-00004.safetensors \
 
 wget https://huggingface.co/DAMO-NLP-SG/VideoLLaMA2-7B/resolve/main/model-00002-of-00004.safetensors?download=true\
-    -O /scratch/project_2010633/videollama2/VideoLLaMA2-7B/model-00002-of-00004.safetensors \
+    -O ${DATA_FOLDER}/VideoLLaMA2-7B/model-00002-of-00004.safetensors \
 
 wget https://huggingface.co/DAMO-NLP-SG/VideoLLaMA2-7B/resolve/main/model-00001-of-00004.safetensors?download=true\
-    -O /scratch/project_2010633/videollama2/VideoLLaMA2-7B/model-00001-of-00004.safetensors \
+    -O ${DATA_FOLDER}/VideoLLaMA2-7B/model-00001-of-00004.safetensors \
