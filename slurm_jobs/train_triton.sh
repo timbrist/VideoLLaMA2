1516f46c6c9d65c1f7c2ba9b6b96037fa254de15
Ulas Gunes <ulas.gunes@aalto.fi>
2024-08-28 16:49:42 +0300

    triton script

---
+++ slurm_jobs/train_triton.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --time=00:10:00
+#SBATCH --mem=500M
+#SBATCH --output=pi-gpu.out
+#SBATCH --gpus=4
+
+module load cuda/12.2.1
+
+export CACHESPACE=$WRKDIR/videollama2_cache
+export HF_DATASETS_CACHE=${CACHESPACE}
+export XDG_CACHE_HOME=${CACHESPACE}
+export PIP_CACHE_DIR=${CACHESPACE}
+export HF_HOME=${CACHESPACE}
+
+
+
+bash ${WORKSPACE}/scripts/vllava/finetune.sh
